35d8481f0e5d192d2ed13c6299a4e4bae5f0b400
TheGreatA <nervous.fiend@gmail.com>
2017-06-25 18:20:03 +0430

    Minor Changes

--- TranslationBot.js
+++ TranslationBot.js
@@ -7,9 +7,7 @@ const translate = require('google-translate-api');
 bot.on('text', function (msg) {
 	translate(msg.text, {to: 'fa'}).then(res => {
     bot.sendMessage(msg.chat.id, res.text)
-    //=> I speak English 
     console.log(res.from.language.iso);
-    //=> nl 
 }).catch(err => {
     console.error(err);
 });
