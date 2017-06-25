c6808ca68e608adb18d4086c0149ed87786f7a75
TheGreatA <nervous.fiend@gmail.com>
2017-06-25 18:18:40 +0430

    Added TranslationBot

---
+++ TranslationBot.js
@@ -0,0 +1,20 @@
+
+const TeleBot = require('telebot');
+const bot = new TeleBot('384552644:AAGllMEVTpKFdMULJ9VRTwAF55ieGvQqWK4');
+const translate = require('google-translate-api');
+
+// Log every text message
+bot.on('text', function (msg) {
+	translate(msg.text, {to: 'fa'}).then(res => {
+    bot.sendMessage(msg.chat.id, res.text)
+    //=> I speak English 
+    console.log(res.from.language.iso);
+    //=> nl 
+}).catch(err => {
+    console.error(err);
+});
+});
+
+
+// Start getting updates
+bot.start();
